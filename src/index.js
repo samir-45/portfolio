2dc6c8785e81e1fe2ef487b57a085c26d0f868aa
samir-45 <mdmahinkhan621@gmail.com>
2026-01-04 13:00:42 +0600

    Some inhancements

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { ReactLenis } from 'lenis/react'; // Import Lenis
 import { ThemeProvider } from './components/theme/theme-provider';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <HelmetProvider>
       <ThemeProvider>
-        <App />
+        {/* Wrap App with ReactLenis for global smooth scroll */}
+        <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+          <App />
+        </ReactLenis>
       </ThemeProvider>
     </HelmetProvider>
   </React.StrictMode>
